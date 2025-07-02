a30ffc1d0460620cc47fc9bac325f01f4ad6a72b
ReginaMichel <regina_michel@outlook.de>
2025-07-02 14:05:25 +0200

    added method to start cars, create and start car in main method of Main class.

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class Car {
+    private String brand;
+    private String model;
+    private String color;
+    private int year;
+
+    public void startCar(){
+        System.out.println("Car starting.");
+    }
 }
